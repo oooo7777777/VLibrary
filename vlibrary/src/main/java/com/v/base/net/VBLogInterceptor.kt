e2aa4d1e70df4c5ef2280aec85e5a0bd1ef8bcec
william <452522426@qq.com>
2022-09-29 11:19:33 +0800

    网络请求日志,把请求url作为tag

--- vlibrary/src/main/java/com/v/base/net/VBLogInterceptor.kt
+++ vlibrary/src/main/java/com/v/base/net/VBLogInterceptor.kt
@@ -2,6 +2,7 @@ package com.v.base.net
 
 import android.text.TextUtils
 import com.alibaba.fastjson.JSONObject
+import com.v.base.utils.log
 import com.v.base.utils.logD
 import com.v.base.utils.logE
 import okhttp3.Headers
@@ -15,6 +16,7 @@ import java.io.IOException
 import java.nio.charset.Charset
 import java.nio.charset.UnsupportedCharsetException
 import java.util.concurrent.TimeUnit
+import okhttp3.HttpUrl
 
 
 /**
@@ -177,7 +179,7 @@ open class VBLogInterceptor(var logTag: String = "PRETTY_LOGGER") : Interceptor 
             sb.append("<-- END HTTP (" + buffer.size() + "-byte body)")
         }
 
-        (sb.toString().replace(", }", "}")).logD(logTag)
+        (sb.toString().replace(", }", "}")).logD("[${request.url()}]")
         return response
     }
 
