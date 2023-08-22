c4ebc701b39709263e37e718cfcbb772c5b92ac1
william <452522426@qq.com>
2023-08-22 16:31:30 +0800

    VBNetApi 添加一些方法

--- vlibrary/src/main/java/com/v/base/net/VBNetApi.kt
+++ vlibrary/src/main/java/com/v/base/net/VBNetApi.kt
@@ -1,5 +1,6 @@
 package com.v.base.net
 
+import okhttp3.RequestBody
 import retrofit2.http.*
 
 interface VBNetApi {
@@ -16,10 +17,17 @@ interface VBNetApi {
     @POST
     suspend fun post(@Url url: String, @Body map: Map<String, @JvmSuppressWildcards Any>): String
 
+    @POST
+    suspend fun post(@Url url: String, @Body body: RequestBody): String
+
     @DELETE
     suspend fun delete(@Url url: String, @QueryMap map:Map<String,  @JvmSuppressWildcards Any>): String
 
     @PUT
     fun put(@Url url: String, @Body map: Map<String, @JvmSuppressWildcards Any>): String
+
+    @PUT
+    suspend fun put(@Url url: String, @Body body: RequestBody): String
+
 
 }
